5a22f00f59d8e2569458bd0ff9cefc93a40cafd4
Hubert Yu <hubyu9234@gmail.com>
2025-11-14 18:50:10 -0800

    initial text delay timers completed

--- src/components/InterfaceManager.tsx
+++ src/components/InterfaceManager.tsx
@@ -1,6 +1,7 @@
 import { useGSAP } from "@gsap/react";
 import SplitText from "gsap/SplitText";
 import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
 
 gsap.registerPlugin(SplitText);
 
@@ -8,26 +9,57 @@ type InterfaceManagerProps = {
   start: boolean;
 };
 
+const test = ["Test 1", "Test 2"];
+
 const InterfaceManager = ({ start }: InterfaceManagerProps) => {
+  const [talking, setTalking] = useState<boolean>(true);
+  const [dialogIndex, setDialogIndex] = useState<number>(0);
+  const firstText = useRef<boolean>(true);
+
+  const [fontReady, setFontReady] = useState<boolean>(true);
+
+  useEffect(() => {
+    document.fonts.ready.then(() => {
+      setFontReady(true);
+    });
+  }, []);
+
+  const textRef = useRef<HTMLSpanElement>(null!);
+
   useGSAP(() => {
+    if (!start || !textRef.current || !fontReady) return;
+
     // split elements with the class "split" into words and characters
-    let split = SplitText.create(".main-text", { type: "words, chars" });
+    let split = SplitText.create(textRef.current, { type: "chars" });
+
+    const delay: number = firstText.current ? 1.5 : 0.3;
 
     gsap.set(split.chars, { opacity: 0 });
     gsap.to(split.chars, {
       opacity: 1,
       duration: 0,
-      stagger: 0.04,
-      delay: 1.5,
+      stagger: 0.06,
+      delay: delay,
+      onComplete: () => {
+        console.log("Text completed");
+        firstText.current = false;
+      },
     });
-  }, [start]);
+  }, [start, dialogIndex]);
+
+  const handleNextText = () => {
+    setDialogIndex(dialogIndex + 1);
+  };
 
   return (
     <div className="absolute bottom-20 left-0 right-0 z-50 flex justify-center">
-      {start && (
-        <div className="hologram px-12 py-6 rounded-md max-w-3xl">
-          <span className="hologram-text text-wrap main-text">
-            Good Morning Victoria, I love youuu
+      {start && talking && (
+        <div
+          className="hologram px-12 py-6 rounded-md w-1/3"
+          onClick={handleNextText}
+        >
+          <span ref={textRef} className="hologram-text text-wrap">
+            {test[dialogIndex]}
           </span>
         </div>
       )}
